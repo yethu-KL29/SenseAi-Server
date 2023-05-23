3bc6bd3347b27ff0ed4f221150c9adc399cb491f
yethu <yethukichu55@gmail.com>
2023-05-23 22:50:09 +0530

    updated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const verify = require('../middleware/verifyToken')
 const { userSignup,userLogin,logout,resetPassword,
-    getAllUsers} = require('../controllers/userController');
+    getAllUsers,
+    loginStatus} = require('../controllers/userController');
 
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
 router.post('/logout', logout);
 router.post('/resetPassword',verify, resetPassword);
-router.get('/getAll', getAllUsers);
+router.get('/getAllUsers', getAllUsers);
+router.get("/loginStatus",loginStatus)
 
 
 
