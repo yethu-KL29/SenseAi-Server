1532ae6cec0a49b21eee4b277731c3a2f8f8b559
yethu <yethukichu55@gmail.com>
2023-05-27 11:00:45 +0530

    test...

--- passportApi.js
+++ passportApi.js
@@ -8,7 +8,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "https://senseai-api.onrender.com/auth/google/login",
+			callbackURL: "https://senseai-api.onrender.com/auth/google/callback",
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
