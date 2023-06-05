c5d520095172b68d8c735f0145bc485228d62556
yethu <yethukichu55@gmail.com>
2023-06-05 22:09:53 +0530

    updated

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,34 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-
-
 const verifyToken = (req, res, next) => {
-    const cookie = req.headers.cookie;
-    if(!cookie){
-        return res.status(401).json({
-            message: 'You need to Authenticate'
-        })
-    }
-    const token = cookie && cookie.split('=')[1];
-    if (!token) {
-        return res.status(401).json({
-            message: 'You need to Authenticate'
-        })
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRETKEY);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Invalid Token'
-        })
-    }
-}
+  const token = req.session.token; // Retrieve the token from the session
 
+  if (!token) {
+    return res.status(401).json({
+      message: 'You need to authenticate',
+    });
+  }
 
-
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRETKEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Invalid token',
+    });
+  }
+};
 
 module.exports = verifyToken;
-
-
